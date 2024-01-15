63964f34dda5646abf4c41fb9420aae2ef7cd43d
Jaouanii <ijaouani.pro@gmail.com>
2024-01-15 02:36:17 +0100

    fix country name in profil

--- backend/controllers/levelsControllers.js
+++ backend/controllers/levelsControllers.js
@@ -15,30 +15,30 @@ const levels = (req, res) => {
 
 const getLevel = (req, res) => {
     const levelId = req.params.id;
-
+  
     getLevelById(levelId)
-        .then(level => {
-            if (!level) {
-                return res.status(404).json({
-                    status: 404,
-                    message: "level not found!!",
-                });
-            }
-
-            return res.status(200).json({
-                status: 200,
-                message: "OK",
-                data: level,
-            });
-        })
-        .catch(error => {
-            return res.status(500).json({
-                status: 500,
-                message: "Internal Server Error",
-                error: error.message,
-            });
+      .then((level) => {
+        if (!level) {
+          return res.status(404).json({
+            status: 404,
+            message: "Level not found!!",
+          });
+        }
+  
+        return res.status(200).json({
+          status: 200,
+          message: "OK",
+          data: level,
         });
-};
-
+      })
+      .catch((error) => {
+        return res.status(500).json({
+          status: 500,
+          message: "Internal Server Error",
+          error: error.message,
+        });
+      });
+  };
+  
 export { levels, getLevel };
 
